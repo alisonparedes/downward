1f660e172a8be093ee0bd1b3da548bf1f6b015ee
Alison Paredes <20asp3@queensu.ca>
2026-06-20 17:40:26 -0400

    Heuristic service accepts state data

--- learned_heuristic_app.py
+++ learned_heuristic_app.py
@@ -1,10 +1,36 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+#from http.server import BaseHTTPRequestHandler, HTTPServer
 
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b"hello")
+#class Handler(BaseHTTPRequestHandler):
+#    def do_GET(self):
+#        self.send_response(200)
+#        self.end_headers()
+#        self.wfile.write(b"hello")
+        
 
-server = HTTPServer(("0.0.0.0", 8080), Handler)
-server.serve_forever()
+#server = HTTPServer(("0.0.0.0", 8080), Handler)
+#server.serve_forever()
+
+# A FastAPI application
+from fastapi import FastAPI, Request
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    #return {"message": "Hello World"}
+    return {"heuristic": 0}
+
+@app.post("/heuristic")
+async def heuristic(payload: dict):
+    
+    logger.info("Received request payload: %s", payload)
+    
+    state = payload["state"]  # Input to learned model
+  
+    h = 0
+
+    return {"heuristic": h}
--- src/search/heuristics/learned_heuristic.cc
+++ src/search/heuristics/learned_heuristic.cc
@@ -5,13 +5,17 @@
 #include "../utils/logging.h"
 
 #include "../ext/httplib.h"
+#include "../ext/json.hpp"
 
 #include <cstddef>
 #include <limits>
 #include <utility>
 
 #include <cassert>
+
+
 using namespace std;
+using json = nlohmann::json;
 
 namespace learned_heuristic {
 // construction and destruction
@@ -32,19 +36,38 @@ int LearnedHeuristic::compute_heuristic(const State &state) { // TODO: What is t
     // 3. Read integer response
     // 4. Return heuristic value
 
+    // 1. Convert state to JSON/string
+    json request_json;
+    
+    state.unpack();
+    request_json["state"] = state.get_unpacked_values();  // TODO: What state representation does Ferber train on?
+
     // 2. POST to web service
     httplib::Client cli("localhost", 8080);
     if (log.is_at_least_normal()) {
         log << "Calling learned heuristic..." << endl;
     }
     // Connectivity test
-    auto response = cli.Get("/");
+    //auto response = cli.Get("/");
+    auto response = cli.Post(
+        "/heuristic",
+        request_json.dump(),
+        "application/json"
+    );
 
     if (response && response->status == 200) {
         if (log.is_at_least_normal()) {
             log << "Response status: 200..." << endl;
         }
-        return 0;
+
+        json response_json = json::parse(response->body);
+
+        int heuristic_value = response_json["heuristic"].get<int>();
+        if (log.is_at_least_normal()) {
+            log << "Heuristic value: " << heuristic_value << endl;
+        }
+
+        return heuristic_value;  
     }
 
 
